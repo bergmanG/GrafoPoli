aec8686e8fa8777de6c6a2ec40f50a8c38a0c177
lordeberg <bergmanguilherme2k@gmail.com>
2019-11-25 23:51:39 -0300

    Update GrafoMatriz.cpp

--- GrafoMatriz.cpp
+++ GrafoMatriz.cpp
@@ -267,27 +267,16 @@ float** GrafoMatriz::prim(int inicial) {
 
 float* GrafoMatriz::bellmanford(int t) {
 	float* M = new float[num_vertices];
-	bool alt = false;
 	for (int i = 0; i < num_vertices; i++) {
 		M[i] = FLT_MAX;
 	}
 	M[t] = 0;
 
 	for (int i = 1; i < num_vertices - 1; i++) {
-		if (i > 1 && !alt) return M;
-		if (M[t] < 0) {
-			ciclo_negativo = true;
-			return M;
-		}
-		alt = false;
 		for (int v = 1; v < num_vertices; v++) {
 			for (int w = 1; w < num_vertices; w++) {
 				if (matriz[v][w]) {
-					int ultimo = M[v];
 					M[v] = min(M[v], M[w] + matrizpeso[v][w]);
-					if (ultimo != M[v]) {
-						alt = true;
-					}
 				}
 			}
 		}
@@ -349,81 +338,90 @@ bool GrafoMatriz::checa_bipartido(int inicial) {
 	bipartido = true;
 	return true;
 }
-list<int>* GrafoMatriz::encontra_caminho(vector<pair<int, bool>>* grafo_residual, bool* expostos) {
-	list<int>* m_aumentante = new list<int>;
-	list<int> pilha;
-	bool* explorado = new bool[num_vertices];
-	int* pai = new int[num_vertices] {0};
-	for (int i = 0; i < conjuntos[0].size(); i++) {
-		int v = conjuntos[0][i];
-		if (expostos[v]) {
-			pilha.push_back(v);
+int GrafoMatriz::hopcroft()
+{
+	int* pairA = new int[num_vertices];
+	int* pairB = new int[num_vertices];
+	int* dist = new int[num_vertices];
+
+	for (int i = 0; i < num_vertices; i++)
+	{
+		pairA[i] = 0;
+	}
+	for (int i = 0; i < num_vertices; i++)
+		pairB[i] = 0;
+
+	int result = 0;
+
+	while (bfsHopcroft(pairA, pairB, dist))
+	{
+		for (int u = 1; u <= conjuntos[0].size(); u++) {
+
+			if (pairA[u] == 0 && dfsHopcroft(u, pairA, pairB, dist))
+				result++;
 		}
 	}
-	bool prox_saturada = false;
-	int v;
-	while (pilha.size() != 0) {
-		fill(explorado, explorado + num_vertices, 0);
-		v = pilha.back();
-		pilha.pop_back();
-		for (int i = 0; i < grafo_residual[v].size(); i++) {
-			if (grafo_residual[v][i].second == prox_saturada) {
-				int r = grafo_residual[v][i].first;
-				if (!explorado[r]) {
-					pilha.push_back(r);
-					pai[r] = v;
-					explorado[r] = 1;
-					if (expostos[r]) {
-						delete[] explorado;
-						expostos[r] = false;
-						do {
-							m_aumentante->push_front(r);
-							r = pai[r];
-						} while (r != 0);
-						expostos[m_aumentante->front()] = false;
-						return m_aumentante;
+	return result;
+}
+bool GrafoMatriz::bfsHopcroft(int* pairA, int* pairB, int* dist)
+{
+	list<int> Q;
+
+	for (int u = 1; u <= conjuntos[0].size(); u++)
+	{
+		if (pairA[u] == 0)
+		{
+			dist[u] = 0;
+			Q.push_back(u);
+		}
+		else dist[u] = INT_MAX;
+	}
+
+	dist[0] = INT_MAX;
+
+	while (!Q.empty())
+	{
+		int u = Q.front();
+		Q.pop_front();
+		if (dist[u] < dist[0])
+		{
+			for (int i = 1; i < num_vertices; i++)
+			{
+				if (matriz[u][i]) {
+					int v = i;
+					if (dist[pairB[v]] == INT_MAX)
+					{
+						dist[pairB[v]] = dist[u] + 1;
+						Q.push_back(pairB[v]);
 					}
 				}
 			}
 		}
-		prox_saturada = !prox_saturada;
 	}
-	delete[]explorado;
-	return 0;
+	return (dist[0] != INT_MAX);
 }
-vector<pair<int, bool>>* GrafoMatriz::hopcraft() {
-	vector<pair<int, bool>>* grafo_residual = new vector<pair<int, bool>>[num_vertices];
-	for (int i = 0; i < conjuntos[0].size(); i++) {
-		int v = conjuntos[0][i];
-		for (int j = 1; j < num_vertices; j++) {
-			if (matriz[v][j]) { 
-				grafo_residual[v].push_back(make_pair(j, false)); 
-			}
-		}
-	}
-	bool* expostos = new bool[num_vertices];
-	fill(expostos, expostos + num_vertices, 1);
-	while (true) {
-		list<int>* m_aumentante = encontra_caminho(grafo_residual, expostos);
-		if (m_aumentante == 0) {
-			break;
-		}
-		auto u = m_aumentante->begin();
-		do {
-			for (int i = 0; i < grafo_residual[*u].size(); i++) {
-				if (grafo_residual[*u][i].first == *(next(u, 1))) {
-					grafo_residual[*u][i].second = !grafo_residual[*u][i].second;
+
+bool GrafoMatriz::dfsHopcroft(int u, int* pairA, int* pairB, int* dist)
+{
+	if (u != 0)
+	{
+		for (int i = 1; num_vertices; i++)
+		{
+			if (matriz[u][i]) {
+				int v = i;
+				if (dist[pairB[v]] == dist[u] + 1)
+				{
+					if (dfsHopcroft(pairB[v], pairA, pairB, dist) == true)
+					{
+						pairB[v] = u;
+						pairA[u] = v;
+						return true;
+					}
 				}
 			}
-			u++;
-		} while (u != --(m_aumentante->end()));
-		delete m_aumentante;
-	}
-	for (int i = 1; i < num_vertices; i++) {
-		if (!expostos[i]) {
-			cout << i << endl;
 		}
+		dist[u] = INT_MAX;
+		return false;
 	}
-	delete[] expostos;
-	return grafo_residual;
+	return true;
 }
